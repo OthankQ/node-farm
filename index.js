eac412339f42f380fd3bc2698f392d3b9075cf3d
ryan <kim1885@purdue.edu>
2020-04-10 11:38:26 -0400

    Asynchronous I/O

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'UTF-8');
-const textOut = `This is what we know about avocado:\n${textIn}.\nCreated on ${Date.now()}`;
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
 
-fs.writeFileSync('./txt/output.txt', textOut);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
 
-console.log('File Created');
+            fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, (err) => {
+                console.log('File written.')
+            })
+        })
+    })
+})
+
+console.log('Will read file...');
