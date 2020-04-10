b3d9c028ed38d5593319a0f714c7106055aafad2
ryan <kim1885@purdue.edu>
2020-04-10 12:04:44 -0400

    Create a server

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const fs = require('fs');
+const http = require('http');
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
+////////////////////////////////
+// File
 
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 
-            fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, (err) => {
-                console.log('File written.')
-            })
-        })
-    })
+//     if (err) return console.log(`There was an error, ${err}`);
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, (err) => {
+//                 console.log('File written.')
+//             })
+//         })
+//     })
+// })
+
+// console.log('Will read file...');
+
+///////////////////////////////
+// Server
+
+const server = http.createServer((req, res) => {
+    res.end('You have reached our server');
 })
+const port = '8000';
 
-console.log('Will read file...');
+server.listen(port, '127.0.0.1', () => {
+    console.log(`Listening to ${port}...`);
+})
